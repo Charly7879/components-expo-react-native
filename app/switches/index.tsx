62f4e27d17e6a334bb4035455f42bf0b83ca227e
Charly <jchar7879@hotmail.com>
2025-12-18 00:02:17 -0300

    Creación de componente Switch

--- app/switches/index.tsx
+++ app/switches/index.tsx
@@ -1,10 +1,42 @@
-import { View, Text } from 'react-native';
+import ThemedCard from '@/presentations/shared/ThemedCard';
+import ThemedSwitch from '@/presentations/shared/ThemedSwitch';
+import ThemedView from '@/presentations/shared/ThemedView';
+import { useState } from 'react';
 
 const Switches = () => {
+
+  // Estados del switch
+  const [state, setState] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true,
+  });
+
   return (
-    <View>
-      <Text>Switches</Text>
-    </View>
+    <ThemedView margin className="mt-3">
+      <ThemedCard>
+        <ThemedSwitch
+          className='mb-2'
+          text='Activo'
+          onValueChange={(value) => setState({ ...state, isActive: value })}
+          value={state.isActive}
+        />
+
+        <ThemedSwitch
+          className='mb-2'
+          text='Hambriento'
+          onValueChange={(value) => setState({ ...state, isHungry: value })}
+          value={state.isHungry}
+        />
+
+        <ThemedSwitch
+          className='mb-2'
+          text='Contento'
+          onValueChange={(value) => setState({ ...state, isHappy: value })}
+          value={state.isHappy}
+        />
+      </ThemedCard>
+    </ThemedView>
   );
 };
 export default Switches;
